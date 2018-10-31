3d72f8e4c00732db43840fb059935a93355a244d
Uladzimir Bachyla <uladzimir.bachyla@gmail.com>
2018-10-31 12:07:06 +0300

    Add info and button + use onClick handler

--- NList/src/components/app/app.js
+++ NList/src/components/app/app.js
@@ -1,76 +1,103 @@
 import React, { Component } from "react";
-import './app.css';
+import PropTypes from "prop-types";
+import "./app.css";
 
 const myNews = [
   {
     id: 1,
     author: "Саша Печкин",
-    text: "В четверг, четвертого числа..."
+    text: "В четверг, четвертого числа...",
+    bigText:
+      "B четыре с четвертью часа четыре чёрненьких чумазеньких чертёнка чертили чёрными чернилами чертёж."
   },
   {
     id: 2,
     author: "Просто Вася",
-    text: "Считаю, что $ должен стоить 35 рублей!"
+    text: "Считаю, что $ должен стоить 35 рублей!",
+    bigText: "А евро 42!"
   },
   {
     id: 3,
     author: "Max Frontend",
-    text: "Прошло 2 года с прошлых учебников, а $ так и не стоит 35"
+    text: "Прошло 2 года с прошлых учебников, а $ так и не стоит 35",
+    bigText: "А евро опять выше 70."
   },
   {
     id: 4,
     author: "Гость",
-    text: "Бесплатно. Без смс, про реакт"
+    text: "Бесплатно. Без смс, про реакт, заходи - https://maxpfrontend.ru",
+    bigText:
+      "Еще есть группа VK, telegram и канал на youtube! Вся инфа на сайте, не реклама!"
   }
 ];
 
 class Article extends Component {
-    render() {
-        const { author, text } = this.props.data
-        return (
-            <div className="article">
-                <p className="news__author">{author}:</p>
-                <p className="news__text">{text}</p>
-            </div>
-        )
+    state = {
+        visible: false,
     }
+
+    handleReadMoreClck = (e) => { 
+        e.preventDefault()
+        this.setState({ visible: true })
+    }
+
+  render() {
+    const { author, text, bigText } = this.props.data;
+    const { visible } = this.state;
+    return (
+      <div className="article">
+        <p className="news__author">{author}:</p>
+        <p className="news__text">{text}</p>
+        { /* если не visible, то показывай */
+          !visible && <a onClick={this.handleReadMoreClck} href="#" className='news__readmore'>Подробнее</a>
+        }
+        { /* если visible, то показывай */
+          visible && <p className='news__big-text'>{bigText}</p>
+        }
+      </div>
+    );
+  }
 }
+Article.propTypes = {
+  data: PropTypes.shape({
+    author: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired,
+    bigText: PropTypes.string.isRequired,
+  })
+};
 
 class News extends Component {
-    renderNews = () => {
-        const { data } = this.props;
-        let newsTemplate = null;
-    
-        if (data.length) {
-          newsTemplate = data.map(item => {
-            return <Article key={item.id} data={item} />
-          });
-        } else {
-          newsTemplate = <p>К сожалению, новостей нет</p>;
-        }
-        return newsTemplate;
+  renderNews = () => {
+    const { data } = this.props;
+    let newsTemplate = null;
+
+    if (data.length) {
+      newsTemplate = data.map(item => {
+        return <Article key={item.id} data={item} />;
+      });
+    } else {
+      newsTemplate = <p>К сожалению, новостей нет</p>;
     }
-    
+    return newsTemplate;
+  };
+
   render() {
     const { data } = this.props;
 
     return (
       <div className="news">
         {this.renderNews()}
-        {
-            data.length ? <strong>Всего новостей: {data.length}</strong> : null
-        }
+        {data.length ? <strong>Всего новостей: {data.length}</strong> : null}
       </div>
     );
   }
 }
 
-
 export default class App extends Component {
   render() {
     return (
       <React.Fragment>
-          <h3>Новости</h3>
+        <h3>Новости</h3>
         <News data={myNews} />
       </React.Fragment>
     );
