5c4ba6016f600daf7fda214a93d290d2fa574ea8
Uladzimir Bachyla <uladzimir.bachyla@gmail.com>
2019-01-10 16:32:44 +0300

    Practice makes perfect means that you need to revise your work whenever you get a chance.

--- Star Wars DB/src/services/swapi-service.js
+++ Star Wars DB/src/services/swapi-service.js
@@ -43,7 +43,7 @@ export default class SwapiService {
       .map(this._transformStarship)
       .slice(0, 10);
   };
-
+ 
   getStarship = async (id) => {
     const starship = await this.getResource(`/starships/${id}/`);
     return this._transformStarship(starship);
