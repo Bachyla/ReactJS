cd47d2c75845d8aebb93aeb35d12caa91d04e63a
Uladzimir Bachyla <uladzimir.bachyla@gmail.com>
2019-02-04 23:43:50 +0300

    Minor style changes

--- Booklist Online/src/components/main/main-column/main-column.js
+++ Booklist Online/src/components/main/main-column/main-column.js
@@ -1,13 +1,17 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React, { Component, Fragment } from 'react';
 
 import './main-column.css';
+import Border1 from '../../../assets/images/header-rl-border.gif';
+
 
 export default class MainColumn extends Component {
     render() {
         return (
         <Fragment>
             <div className="main-column">
-                
+                <img src={Border1} className="left-border"/>
+                <img src={Border1} className="right-border"/>
             </div>
         </Fragment>  
         );
