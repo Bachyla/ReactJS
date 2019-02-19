5f41c17c177befa14a188a85820bdcd5fbc5b336
Uladzimir Bachyla <uladzimir.bachyla@gmail.com>
2019-02-19 10:05:58 +0300

    refactor: Add newlines between operands of ternary expression in 'HomeComponent'

--- csr/src/components/HomeComponent.js
+++ csr/src/components/HomeComponent.js
@@ -21,7 +21,9 @@ function RenderCard({ item, isLoading, errMess }) {
                 <CardImg src={baseUrl + item.image} alt={item.name} />
                 <CardBody>
                     <CardTitle>{item.name}</CardTitle>
-                    {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null}
+                        {item.designation ? 
+                        <CardSubtitle>{item.designation}</CardSubtitle> :
+                         null}
                     <CardText>{item.description}</CardText>
                 </CardBody>
             </Card>
