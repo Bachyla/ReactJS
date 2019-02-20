bbf9abc082df4f4d75a134f88117eeb3a2685b52
Uladzimir Bachyla <uladzimir.bachyla@gmail.com>
2019-02-20 10:29:51 +0300

    refactor: Update styles under mostly reasonable approach to CSS - Airbnb

--- Booklist Online/src/components/share-buttons/share-buttons.js
+++ Booklist Online/src/components/share-buttons/share-buttons.js
@@ -10,33 +10,33 @@ import PinterestIcon from '../../assets/images/social/pinterest.svg';
 
 const ShareButtons = () => {
     return (
-            <div className="share-btns">
+            <div className="share-buttons-wrapper">
                     <a href="/">
-                        <div className="icon-wrapper border-radius-0 f-color">
+                        <div className="icon-wrapper border-radius-0 facebook-color">
                             <img src={FacebookIcon} alt="Facebook" width="22" height="22"/>
                         </div>
                     </a>
 
                     <a href="/">
-                        <div className="icon-wrapper border-radius-0 t-color">
-                            <img src={TwitterIcon} alt="Twitter" width="22" height="22"/>
-                        </div>
-                    </a>
-
-                    <a href="/">
-                        <div className="icon-wrapper border-radius-0 p-color">
+                        <div className="icon-wrapper border-radius-0 print-color">
                             <img src={PrinterIcon} alt="Twitter" width="22" height="22"/>
                         </div>
                     </a>
 
                     <a href="/">
-                        <div className="icon-wrapper border-radius-0 m-color">
+                        <div className="icon-wrapper border-radius-0 twitter-color">
+                            <img src={TwitterIcon} alt="Twitter" width="22" height="22"/>
+                        </div>
+                    </a>
+
+                    <a href="/">
+                        <div className="icon-wrapper border-radius-0 mail-color">
                             <img src={MailIcon} alt="Mail" width="22" height="22"/>
                         </div>
                     </a>
 
                     <a href="/">
-                        <div className="icon-wrapper border-radius-0 pint-color">
+                        <div className="icon-wrapper border-radius-0 pinterest-color">
                             <img src={PinterestIcon} alt="Pinteresr" width="22" height="22"/>
                         </div>
                     </a>
