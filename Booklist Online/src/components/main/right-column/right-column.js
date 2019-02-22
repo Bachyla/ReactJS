159884077d9b8bebfc367c236feb6bfcc2262ebc
Uladzimir Bachyla <uladzimir.bachyla@gmail.com>
2019-02-22 08:52:21 +0300

    delete repo

--- Booklist Online/src/components/main/right-column/right-column.js
+++ Booklist Online/src/components/main/right-column/right-column.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 import './right-column.css';
 
-import FollowButtons from './follow-buttons'
+import FollowButtons from './follow-buttons';
+
+import AdvertisingImage from '../../../assets/images/advertising/right-column-banner.png'
 
 const RightColumn = () => {
     return (
         <div className="right-column">
             <FollowButtons />
+            <a href="https://bls.pcdfusion.com/pcd/Order?iKey=I**MW8" title="Click here to subscribe!"><img src={AdvertisingImage} className="rc-adverting-img" alt="Click here to subscribe!" width="160" height="600"></img></a>
         </div>
     );
 };
