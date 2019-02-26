d8442e4651ca6a671a61ef56c8b209bec8c70f18
Uladzimir Bachyla <uladzimir.bachyla@gmail.com>
2019-02-26 15:40:33 +0300

    feat: complete saved-searches page

--- Booklist Online/src/components/main/main-column/pages/saved-searches.js
+++ Booklist Online/src/components/main/main-column/pages/saved-searches.js
@@ -8,7 +8,92 @@ class SavedSearches extends Component {
             <Fragment>
                 <NavAdvancedSearch ssActive={true}/>
                 <div className="search-wrapper">
-                    <h3 className="search-title grey-border">Saved Searches: Amanda</h3>
+                    <h3 className="search-title grey-border margin-bottom-0">Saved Searches: Amanda</h3>
+                    <span className="annotation">Number of saved searches: 10</span>
+                    
+                    <div className="saved-search-results-wrapper">
+                        <span className="saved-search-lable">Name:</span>
+                        <span className="saved-search-lable">Date Created:</span>
+                        <span className="saved-search-lable">Last Updated:</span>
+                    </div>
+
+                    <div className="saved-search-results-wrapper ss-style margin-bottom-50">
+                        <span className="ss-table-text">Search for 1.1</span>
+                        <span className="ss-table-text">5/27/2010</span>
+                        <span className="ss-table-text">5/27/2010</span>
+                    </div>
+
+                    <div className="saved-search-results-wrapper margin-bottom-15 ss-links-wrapper">
+                        <a href="/" class="clear-form-button"><u>RUN SEARCH</u></a>
+                        <a href="/" class="clear-form-button"><u>VIEW/EDIT SEARCH</u></a>
+                        <a href="/" class="clear-form-button"><u>RENAME</u></a>
+                        <a href="/" class="clear-form-button"><u>DELETE</u></a>
+                    </div>
+
+                    <div className="saved-search-results-wrapper ss-style margin-bottom-50">
+                        <span className="ss-table-text">Search for girl</span>
+                        <span className="ss-table-text">5/26/2010</span>
+                        <span className="ss-table-text">5/26/2010</span>
+                    </div>
+
+                    <div className="saved-search-results-wrapper margin-bottom-15 ss-links-wrapper">
+                        <a href="/" class="clear-form-button"><u>RUN SEARCH</u></a>
+                        <a href="/" class="clear-form-button"><u>VIEW/EDIT SEARCH</u></a>
+                        <a href="/" class="clear-form-button"><u>RENAME</u></a>
+                        <a href="/" class="clear-form-button"><u>DELETE</u></a>
+                    </div>
+
+                    <div className="saved-search-results-wrapper ss-style margin-bottom-50">
+                        <span className="ss-table-text">Search for dog</span>
+                        <span className="ss-table-text">5/25/2010</span>
+                        <span className="ss-table-text">5/25/2010</span>
+                    </div>
+
+                    <div className="saved-search-results-wrapper margin-bottom-15 ss-links-wrapper">
+                        <a href="/" class="clear-form-button"><u>RUN SEARCH</u></a>
+                        <a href="/" class="clear-form-button"><u>VIEW/EDIT SEARCH</u></a>
+                        <a href="/" class="clear-form-button"><u>RENAME</u></a>
+                        <a href="/" class="clear-form-button"><u>DELETE</u></a>
+                    </div>
+
+                    <div className="saved-search-results-wrapper ss-style margin-bottom-50">
+                        <span className="ss-table-text">Search for cocaine</span>
+                        <span className="ss-table-text">5/24/2010</span>
+                        <span className="ss-table-text">5/24/2010</span>
+                    </div>
+
+                    <div className="saved-search-results-wrapper margin-bottom-15 ss-links-wrapper">
+                        <a href="/" class="clear-form-button"><u>RUN SEARCH</u></a>
+                        <a href="/" class="clear-form-button"><u>VIEW/EDIT SEARCH</u></a>
+                        <a href="/" class="clear-form-button"><u>RENAME</u></a>
+                        <a href="/" class="clear-form-button"><u>DELETE</u></a>
+                    </div>
+
+                    <div className="saved-search-results-wrapper ss-style margin-bottom-50">
+                        <span className="ss-table-text">Search for science</span>
+                        <span className="ss-table-text">5/24/2010</span>
+                        <span className="ss-table-text">5/24/2010</span>
+                    </div>
+
+                    <div className="saved-search-results-wrapper margin-bottom-15 ss-links-wrapper">
+                        <a href="/" class="clear-form-button"><u>RUN SEARCH</u></a>
+                        <a href="/" class="clear-form-button"><u>VIEW/EDIT SEARCH</u></a>
+                        <a href="/" class="clear-form-button"><u>RENAME</u></a>
+                        <a href="/" class="clear-form-button"><u>DELETE</u></a>
+                    </div>
+
+                    <div className="saved-search-results-wrapper ss-style margin-bottom-50">
+                        <span className="ss-table-text">Search for harry potter</span>
+                        <span className="ss-table-text">5/24/2010</span>
+                        <span className="ss-table-text">5/24/2010</span>
+                    </div>
+
+                    <div className="saved-search-results-wrapper margin-bottom-15 ss-links-wrapper">
+                        <a href="/" class="clear-form-button"><u>RUN SEARCH</u></a>
+                        <a href="/" class="clear-form-button"><u>VIEW/EDIT SEARCH</u></a>
+                        <a href="/" class="clear-form-button"><u>RENAME</u></a>
+                        <a href="/" class="clear-form-button"><u>DELETE</u></a>
+                    </div>
                 </div>
             </Fragment>
         )
--- csr/components/DishDetailComponent.js
+++ csr/components/DishDetailComponent.js
@@ -1,194 +1,258 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, FlatList, Modal, StyleSheet,Button } from 'react-native';
+import { Text, View, ScrollView, FlatList, Modal, StyleSheet, Button, Alert, PanResponder, Share } from 'react-native';
 import { Card, Icon, Rating, Input } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
-import { postFavorite,postComment } from '../redux/ActionCreators';
+import { postFavorite, postComment } from '../redux/ActionCreators';
+import * as Animatable from 'react-native-animatable';
 
 const mapStateToProps = state => {
     return {
-        dishes: state.dishes,
-        comments: state.comments,
-        favorites: state.favorites
+      dishes: state.dishes,
+      comments: state.comments,
+      favorites: state.favorites
     }
 }
 
 const mapDispatchToProps = dispatch => ({
     postFavorite: (dishId) => dispatch(postFavorite(dishId)),
-    postComment: (dishId,rating,author,comment) => dispatch(postComment(dishId,rating,author,comment))
-})
-
-function RenderComments(props) {
-
-    const comments = props.comments;
-
-    const renderCommentItem = ({ item, index }) => {
-
-        return (
-            <View key={index} style={{ margin: 10 }}>
-                <Text style={{ fontSize: 14 }}>{item.comment}</Text>
-                <Text style={{ fontSize: 12 }}>{item.rating} Stars</Text>
-                <Text style={{ fontSize: 12 }}>{'-- ' + item.author + ', ' + item.date} </Text>
-            </View>
-        );
-    };
-
-    return (
-        <Card title='Comments' >
-            <FlatList
-                data={comments}
-                renderItem={renderCommentItem}
-                keyExtractor={item => item.id.toString()}
-            />
-        </Card>
-    );
-}
+    postComment: (dishId, rating, author, comment) => dispatch(postComment(dishId, rating, author, comment))  
+});
 
 function RenderDish(props) {
 
     const dish = props.dish;
 
+    handleViewRef = ref => this.view = ref;
+
+    const recognizeDrag = ({ moveX, moveY, dx, dy }) => {
+        if ( dx < -200 )
+            return true;
+        else
+            return null;
+    };
+    
+    const recognizeComment = ({ moveX, moveY, dx, dy }) => {
+        if ( dx > 200 )
+            return true;
+        else
+            return null;
+    };
+
+    const panResponder = PanResponder.create({
+        onStartShouldSetPanResponder: (e, gestureState) => {
+            return true;
+        },
+        onPanResponderGrant: () => {
+            this.view.rubberBand(1000).then(endState => console.log(endState.finished ? 'finished' : 'cancelled'));
+        },
+
+        onPanResponderEnd: (e, gestureState) => {
+            console.log("pan responder end", gestureState);
+            if (recognizeDrag(gestureState)) 
+                Alert.alert(
+                    'Add Favorite',
+                    'Are you sure you wish to add ' + dish.name + ' to favorite?',
+                    [
+                        {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+                        {text: 'OK', onPress: () => {props.favorite ? console.log('Already favorite') : props.onPress()}},
+                    ],
+                    { cancelable: false }
+                );
+            else if (recognizeComment(gestureState))
+                props.showModal();
+            return true;
+        }
+    });
+
+    const shareDish = (title, message, url) => {
+        Share.share({
+            title: title,
+            message: title + ': ' + message + ' ' + url,
+            url: url
+        },{
+            dialogTitle: 'Share ' + title
+        })
+    }
+    
     if (dish != null) {
-        return (
-            <Card
-                featuredTitle={dish.name}
-                image={{ uri: baseUrl + dish.image }}>
-                <Text style={{ margin: 10 }}>
-                    {dish.description}
-                </Text>
-                <View style={styles.formRow}>
-                    <Icon
-                        raised
-                        reverse
-                        name={props.favorite ? 'heart' : 'heart-o'}
-                        type='font-awesome'
-                        color='#f50'
-                        onPress={() => props.favorite ? console.log('Already favorite') : props.onPress()}
-                    />
-                    <Icon
-                        raised
-                        reverse
-                        name={'pencil'}
-                        type='font-awesome'
-                        color="#512DA8"
-                        onPress={() => props.onComment()}
-                    />
-                </View>
-            </Card>
+        return(
+                <Animatable.View animation="fadeInDown" duration={2000} delay={1000}
+                    ref={this.handleViewRef}
+                    {...panResponder.panHandlers}>
+                    <Card
+                        featuredTitle={dish.name}
+                        image={{uri: baseUrl + dish.image}}
+                        >
+                        <Text style={{margin: 10}}>
+                            {dish.description}
+                        </Text>
+                        <View style={{flex: 1, flexDirection: 'row'}}>
+                        <Icon   
+                            raised
+                            reverse
+                            name={ props.favorite ? 'heart' : 'heart-o' }
+                            type='font-awesome'
+                            color='#f50'
+                            onPress={() => props.favorite ? console.log('Already favorite') : props.onPress()}
+                            />
+                        <Icon     
+                            raised
+                            reverse
+                            name={ 'pencil' }
+                            type='font-awesome'
+                            color='#512DA8'
+                            onPress={() => { props.showModal()}}
+                            />
+                        <Icon
+                            raised
+                            reverse
+                            name='share'
+                            type='font-awesome'
+                            color='#51D2A8'
+                            onPress={() => shareDish(dish.name, dish.description, baseUrl + dish.image)} />
+                        </View>    
+                    </Card>
+                </Animatable.View>
         );
     }
-    else {
-        return (<View></View>);
+     else {
+        return(<View></View>);
     }
 }
 
+function RenderComments(props) {
+    const comments = props.comments;
+
+    const renderCommentItem = ({ item, index }) => {
+        return(
+            <View key={index} style={{margin: 10}}>
+                <Text style={{fontSize: 14}}>{item.comment}</Text>
+                <Rating
+                    startingValue={parseInt(item.rating,10)}
+                    readonly
+                    imageSize={15}
+                    style={{ paddingVertical: 5 }}
+                />
+                <Text style={{fontSize: 12}}>{'--' + item.author + ', ' + item.date}</Text>
+            </View>
+        );
+    };
+
+    return(
+        <Animatable.View animation="fadeInUp" duration={2000} delay={1000}>
+            <Card title="Comments">
+                <FlatList
+                    data={comments}
+                    renderItem={renderCommentItem}
+                    keyExtractor={item => item.id.toString()}
+                    />
+            </Card>
+        </Animatable.View>
+    )
+}
 
 class Dishdetail extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            rating: 3,
+            rating: 5,
             author: '',
-            comment_text: '',
+            comment: '',
             showModal: false
         };
+    }
+    
+    markFavorite(dishId) {
+        this.props.postFavorite(dishId);
     }
 
     static navigationOptions = {
         title: 'Dish Details'
     };
 
-    markFavorite(dishId) {
-        this.props.postFavorite(dishId);
-    }
-
     toggleModal() {
-        this.setState({ showModal: !this.state.showModal });
-    }
-   
-   
-
-    handleComment(dishId) {
-        console.log(JSON.stringify(this.state));
-        this.props.postComment(dishId,this.state.rating,this.state.author,this.state.comment_text)
-        this.resetComment()
-        this.toggleModal()
+        this.setState({ showModal: !this.state.showModal })
     }
 
-    resetComment() {
+    handleComment(dishId, rating, author, comment) {
+        this.props.postComment(dishId, rating, author, comment);
+        this.toggleModal();
+        this.resetForm();
+    }
+
+    resetForm() {
         this.setState({
-            rating: 3,
+            rating: 5,
             author: '',
-            comment_text: '',
+            comment: '',
             showModal: false
         });
     }
-
-
+    
     render() {
         const dishId = this.props.navigation.getParam('dishId', '');
-        return (
-            <ScrollView>
-                <RenderDish dish={this.props.dishes.dishes[+dishId]}
-                    favorite={this.props.favorites.some(el => el === dishId)}
-                    onPress={() => this.markFavorite(dishId)}
-                    onComment={() => this.toggleModal()}
+
+        return(
+        <ScrollView>
+            <RenderDish dish={this.props.dishes.dishes[+dishId]}
+                favorite={this.props.favorites.some(el => el === dishId)}
+                onPress={() => this.markFavorite(dishId)}
+                showModal={() => {this.toggleModal()}}
                 />
-                <RenderComments comments={this.props.comments.comments.filter((comment) => comment.dishId === dishId)} />
-
-                <Modal animationType={"slide"} transparent={false}
+            <RenderComments comments={this.props.comments.comments.filter((comment) => comment.dishId === dishId)} />
+            <Modal
+                    animationType={'slide'}
+                    transparent={false}
                     visible={this.state.showModal}
-                    onDismiss={() => this.toggleModal()}
-                    onRequestClose={() => this.toggleModal()}>
-                    <ScrollView>
-                        <View style={styles.formRow}>
-                            <Rating  defaultRating={3}  showRating  type="star"    fractions={0}     
-                                onFinishRating={(itemValue) => this.setState({ rating: itemValue })}
-
+                    onDismiss={() => {this.toggleModal(); this.resetForm()}}
+                    onRequestClose={() => {this.toggleModal(); this.resetForm()}}
+                    >
+                    <View style={styles.modal}>
+                    <Rating
+                        type="star"
+                        ratingCount={5}
+                        fractions={0}
+                        startingValue={5}
+                        imageSize={40}
+                        onFinishRating={(rating) => {this.setState({rating: rating})}}
+                        showRating
+                        style={{ paddingVertical: 10 }}
+                        />
+                        <Input
+                            placeholder='Author'
+                            leftIcon={{ type: 'font-awesome', name: 'user-o' }}
+                            onChangeText={(text) => this.setState({author: text})}
+                        />
+                        <Input
+                            placeholder='Comment'
+                            leftIcon={{ type: 'font-awesome', name: 'comment-o' }}
+                            onChangeText={(text) => this.setState({comment: text})}
+                        />
+                        <Button 
+                            onPress = {() =>{this.handleComment(dishId,this.state.rating,this.state.author,this.state.comment); this.resetForm()}}
+                            color="#512DA8"
+                            title="SUBMIT" 
                             />
-                        </View>
-                        <View style={styles.formRow}>
-                            <Input
-                                placeholder='Author'
-                                leftIcon={{ type: 'font-awesome', name: 'user' }}
-                                onChangeText={(itemValue) => this.setState({ author: itemValue })}
+                        <Button 
+                            onPress = {() =>{this.toggleModal(); this.resetForm()}}
+                            color="#ccc"
+                            title="CANCEL" 
                             />
-                        </View>
-                        <View style={styles.formRow}>
-                            <Input
-                                placeholder='Comment'
-                                leftIcon={{ type: 'font-awesome', name: 'comment' }}
-                                onChangeText={(itemValue) => this.setState({ comment_text: itemValue })}
-                            />
-                        </View>
-                        <View style={styles.formRow}>
-                            <Button
-                                onPress={() => this.handleComment(dishId)}
-                                title="SUBMIT"
-                                color="#512DA8"
-                            />
-                        </View>
-                        <View style={styles.formRow}>
-                            <Button
-                                onPress={() => this.resetComment()}
-                                title="CANCEL"
-                                color="#C0C0C0"
-                            />
-                        </View>
-                    </ScrollView>
-                </Modal>
-            </ScrollView>
+                    </View>
+            </Modal>
+        </ScrollView>
         );
     }
 }
 
 const styles = StyleSheet.create({
     formRow: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        flex: 1,
-        flexDirection: 'row',
-        margin: 20
+      alignItems: 'center',
+      justifyContent: 'center',
+      flex: 1,
+      flexDirection: 'row',
+      margin: 20
     },
     formLabel: {
         fontSize: 18,
@@ -214,6 +278,5 @@ const styles = StyleSheet.create({
         margin: 10
     }
 });
-
 
 export default connect(mapStateToProps, mapDispatchToProps)(Dishdetail);
